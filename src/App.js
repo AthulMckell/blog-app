a1fd3a6c21c61da3ab8b106278df9b4c07f09c78
Athul Raj <athulraj786@gmail.com>
2022-02-11 10:25:08 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import ArticleList from './components/article/ArticleList';
+import Article from './components/article/Article';
+import Error from './components/error/Error';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path = '/about' element={<About />} />
+        <Route path='/articlelist' element={< ArticleList />} />
+        <Route path='/article/:name' element={<Article />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+   </>
+   </Router>
   );
 }
 
